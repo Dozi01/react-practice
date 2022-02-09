2d828c46d7cbb891700d41a81d57054564557213
Dozi <ytooom019@naver.com>
2022-02-09 13:11:37 +0900

    useEffect cleanup function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setvalue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setvalue((prev) => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  console.log('I run all time');
+function Hello() {
   useEffect(() => {
-    console.log('I run only once');
+    console.log('created :)');
+    return () => console.log('destroyed :(');
   }, []);
-  useEffect(() => {
-    if (keyword !== '' && keyword.length >= 5) {
-      console.log('SEARCH FOR', keyword);
-    }
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here.."
-      />
-
-      <h1 className={styles.title}>Welcome back! counter : {counter}</h1>
-      <button onClick={onClick}> button</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
