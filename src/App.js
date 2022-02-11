928a7542190e1fa5532c3e5c2d0877901343355b
Dozi <ytooom019@naver.com>
2022-02-11 23:39:50 +0900

    router connect link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-import Movie from "./components/movie";
 import Home from "./routes/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
