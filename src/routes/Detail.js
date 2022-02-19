14cf6640c4f0b0235e81cef61ffcf48654263a95
Dozi <ytooom019@naver.com>
2022-02-19 13:04:26 +0900

    movie detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-
-  return <h1>Detail</h1>;
+  console.log(movie);
+  return (
+    <div>
+      <h1>Detail</h1>
+      <h2>Movie : {movie.title}</h2>
+    </div>
+  );
 }
 
 export default Detail;
