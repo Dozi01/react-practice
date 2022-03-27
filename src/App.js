0655b383f340a01844453ee14b1bc32916d0bb6d
Dozi <ytooom019@naver.com>
2022-03-27 21:51:54 +0900

    github deploy

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import { useState, useEffect } from "react";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import Group from "./routes/Group";
-
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 import styles from "./styles.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
